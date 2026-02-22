timvisher-dd/agent-shell

Stabilize test expectations
Co-authored-by: Codex <codex@openai.com>

tests/agent-shell-tests.el
@@ -191,21 +191,21 @@
   (dolist (test-case `(;; Graphical display mode
                        ( :graphic t
                          :homogeneous-expected
-                         ,(concat " pending   Update state initialization\n"
-                                  " pending   Update session initialization")
+                         ,(concat " pending  Update state initialization\n"
+                                  " pending  Update session initialization")
                          :mixed-expected
-                         ,(concat " pending       First task\n"
-                                  " in progress   Second task\n"
-                                  " completed     Third task"))
+                         ,(concat " pending      First task\n"
+                                  " in progress  Second task\n"
+                                  " completed    Third task"))
                        ;; Terminal display mode
                        ( :graphic nil
                          :homogeneous-expected
-                         ,(concat "[pending]  Update state initialization\n"
-                                  "[pending]  Update session initialization")
+                         ,(concat "[pending] Update state initialization\n"
+                                  "[pending] Update session initialization")
                          :mixed-expected
-                         ,(concat "[pending]      First task\n"
-                                  "[in progress]  Second task\n"
-                                  "[completed]    Third task"))))
+                         ,(concat "[pending]     First task\n"
+                                  "[in progress] Second task\n"
+                                  "[completed]   Third task"))))
     (cl-letf (((symbol-function 'display-graphic-p)
                (lambda (&optional _display) (plist-get test-case :graphic))))
       ;; Test homogeneous statuses
@@ -376,7 +376,9 @@
 
           ;; Mock agent-shell-cwd
           (cl-letf (((symbol-function 'agent-shell-cwd)
-                     (lambda () default-directory)))
+                     (lambda () default-directory))
+                    ((symbol-function 'agent-shell--image-type-to-mime)
+                     (lambda (_filename) "image/png")))
 
             ;; Test with image and embedded context support - should use ContentBlock::Image
             (let ((agent-shell--state (list
@@ -389,7 +391,6 @@
                 (should (equal (map-elt (nth 0 blocks) 'type) "text"))
                 (should (equal (map-elt (nth 0 blocks) 'text) "Analyze"))
 
-                ;; Check image block
                 (let ((image-block (nth 1 blocks)))
                   (should (equal (map-elt image-block 'type) "image"))
 
@@ -474,7 +475,9 @@
     ;; Mock acp-send-request to capture what gets sent
     (cl-letf (((symbol-function 'acp-send-request)
                (lambda (&rest args)
-                 (setq sent-request args))))
+                 (setq sent-request args)))
+              ((symbol-function 'agent-shell-viewport--buffer)
+               (lambda (&rest _args) nil)))
 
       ;; Send a simple command
       (agent-shell--send-command
@@ -507,7 +510,9 @@
                  (error "Simulated error in build-content-blocks")))
               ((symbol-function 'acp-send-request)
                (lambda (&rest args)
-                 (setq sent-request args))))
+                 (setq sent-request args)))
+              ((symbol-function 'agent-shell-viewport--buffer)
+               (lambda (&rest _args) nil)))
 
       ;; First, verify that build-content-blocks actually throws an error
       (should-error (agent-shell--build-content-blocks "Test prompt")
@@ -1149,25 +1154,30 @@ code block content
 
 (ert-deftest agent-shell--format-session-date-test ()
   "Test `agent-shell--format-session-date' humanizes timestamps."
-  ;; Today
-  (let* ((now (current-time))
-         (today-iso (format-time-string "%Y-%m-%dT10:30:00Z" now)))
-    (should (equal (agent-shell--format-session-date today-iso)
-                   "Today, 10:30")))
-  ;; Yesterday
-  (let* ((yesterday (time-subtract (current-time) (* 24 60 60)))
-         (yesterday-iso (format-time-string "%Y-%m-%dT15:45:00Z" yesterday)))
-    (should (equal (agent-shell--format-session-date yesterday-iso)
-                   "Yesterday, 15:45")))
-  ;; Same year, older
-  (should (string-match-p "^[A-Z][a-z]+ [0-9]+, [0-9]+:[0-9]+"
-                           (agent-shell--format-session-date "2026-01-05T09:00:00Z")))
-  ;; Different year
-  (should (string-match-p "^[A-Z][a-z]+ [0-9]+, [0-9]\\{4\\}"
-                           (agent-shell--format-session-date "2025-06-15T12:00:00Z")))
-  ;; Invalid input falls back gracefully
-  (should (equal (agent-shell--format-session-date "not-a-date")
-                 "not-a-date")))
+  (let ((fixed-now (date-to-time "2026-02-21T12:00:00Z")))
+    (cl-letf (((symbol-function 'current-time) (lambda () fixed-now)))
+      ;; Today
+      (let* ((today-time (date-to-time "2026-02-21T10:30:00Z"))
+             (today-expected (format-time-string "Today, %H:%M" today-time)))
+        (should (equal (agent-shell--format-session-date "2026-02-21T10:30:00Z")
+                       today-expected)))
+      ;; Yesterday
+      (let* ((yesterday-time (date-to-time "2026-02-20T15:45:00Z"))
+             (yesterday-expected (format-time-string "Yesterday, %H:%M" yesterday-time)))
+        (should (equal (agent-shell--format-session-date "2026-02-20T15:45:00Z")
+                       yesterday-expected)))
+      ;; Same year, older
+      (should (string-match-p "^[A-Z][a-z]+ [0-9]+, [0-9]+:[0-9]+"
+                              (agent-shell--format-session-date "2026-01-05T09:00:00Z")))
+      ;; Different year
+      (should (string-match-p "^[A-Z][a-z]+ [0-9]+, [0-9]\\{4\\}"
+                              (agent-shell--format-session-date "2025-06-15T12:00:00Z")))
+      ;; Invalid input falls back gracefully
+      (cl-letf (((symbol-function 'date-to-time)
+                 (lambda (_timestamp)
+                   (error "Unparseable timestamp"))))
+        (should (equal (agent-shell--format-session-date "not-a-date")
+                       "not-a-date"))))))
 
 (ert-deftest agent-shell--prompt-select-session-test ()
   "Test `agent-shell--prompt-select-session' choices."
@@ -1342,7 +1352,7 @@ code block content
   (should (null (agent-shell--extract-tool-parameters
                  '((plan . "Step 1: do something"))))))
 
-(ert-deftest agent-shell--make-transcript-tool-call-entry-test ()
+(ert-deftest agent-shell--make-transcript-tool-call-entry-parameters-test ()
   "Test `agent-shell--make-transcript-tool-call-entry' with parameters."
   ;; Test basic entry without parameters
   (let ((entry (agent-shell--make-transcript-tool-call-entry

timvisher-dd/agent-shell-plus

tests/agent-shell-tests.el
@@ -188,33 +188,38 @@
 
 (ert-deftest agent-shell--format-plan-test ()
   "Test `agent-shell--format-plan' function."
-  (dolist (test-case `(;; Graphical display mode
-                       ( :graphic t
-                         :homogeneous-expected
-                         ,(concat " wait  Update state initialization\n"
-                                  " wait  Update session initialization")
-                         :mixed-expected
-                         ,(concat " wait  First task\n"
-                                  " busy  Second task\n"
-                                  " done  Third task"))
-                       ;; Terminal display mode
-                       ( :graphic nil
-                         :homogeneous-expected
-                         ,(concat "[wait] Update state initialization\n"
-                                  "[wait] Update session initialization")
-                         :mixed-expected
-                         ,(concat "[wait] First task\n"
-                                  "[busy] Second task\n"
-                                  "[done] Third task"))))
-    (cl-letf (((symbol-function 'display-graphic-p)
-               (lambda (&optional _display) (plist-get test-case :graphic))))
-      ;; Test homogeneous statuses
-      (should (equal (substring-no-properties
-                      (agent-shell--format-plan [((content . "Update state initialization")
-                                                  (status . "pending"))
-                                                 ((content . "Update session initialization")
-                                                  (status . "pending"))]))
-                     (plist-get test-case :homogeneous-expected)))
+  (let ((agent-shell-status-labels
+         '(("pending" . ((:label . "wait") (:face . font-lock-comment-face)))
+           ("in_progress" . ((:label . "busy") (:face . warning)))
+           ("completed" . ((:label . "done") (:face . success)))
+           ("failed" . ((:label . "error") (:face . error))))))
+    (dolist (test-case `(;; Graphical display mode
+                         ( :graphic t
+                           :homogeneous-expected
+                           ,(concat " wait  Update state initialization\n"
+                                    " wait  Update session initialization")
+                           :mixed-expected
+                           ,(concat " wait  First task\n"
+                                    " busy  Second task\n"
+                                    " done  Third task"))
+                         ;; Terminal display mode
+                         ( :graphic nil
+                           :homogeneous-expected
+                           ,(concat "[wait] Update state initialization\n"
+                                    "[wait] Update session initialization")
+                           :mixed-expected
+                           ,(concat "[wait] First task\n"
+                                    "[busy] Second task\n"
+                                    "[done] Third task"))))
+      (cl-letf (((symbol-function 'display-graphic-p)
+                 (lambda (&optional _display) (plist-get test-case :graphic))))
+        ;; Test homogeneous statuses
+        (should (equal (substring-no-properties
+                        (agent-shell--format-plan [((content . "Update state initialization")
+                                                    (status . "pending"))
+                                                   ((content . "Update session initialization")
+                                                    (status . "pending"))]))
+                       (plist-get test-case :homogeneous-expected)))
 
       ;; Test mixed statuses
       (should (equal (substring-no-properties
@@ -227,7 +232,7 @@
                      (plist-get test-case :mixed-expected)))))
 
   ;; Test empty entries
-  (should (equal (agent-shell--format-plan []) "")))
+  (should (equal (agent-shell--format-plan []) ""))))
 
 (ert-deftest agent-shell--make-button-test ()
   "Test `agent-shell--make-button' brackets in terminal mode."
@@ -376,7 +381,9 @@
 
           ;; Mock agent-shell-cwd
           (cl-letf (((symbol-function 'agent-shell-cwd)
-                     (lambda () default-directory)))
+                     (lambda () default-directory))
+                    ((symbol-function 'agent-shell--image-type-to-mime)
+                     (lambda (_filename) "image/png")))
 
             ;; Test with image and embedded context support - should use ContentBlock::Image
             (let ((agent-shell--state (list
@@ -389,7 +396,6 @@
                 (should (equal (map-elt (nth 0 blocks) 'type) "text"))
                 (should (equal (map-elt (nth 0 blocks) 'text) "Analyze"))
 
-                ;; Check image block
                 (let ((image-block (nth 1 blocks)))
                   (should (equal (map-elt image-block 'type) "image"))
 
@@ -474,7 +480,9 @@
     ;; Mock acp-send-request to capture what gets sent
     (cl-letf (((symbol-function 'acp-send-request)
                (lambda (&rest args)
-                 (setq sent-request args))))
+                 (setq sent-request args)))
+              ((symbol-function 'agent-shell-viewport--buffer)
+               (lambda (&rest _args) nil)))
 
       ;; Send a simple command
       (agent-shell--send-command
@@ -507,7 +515,9 @@
                  (error "Simulated error in build-content-blocks")))
               ((symbol-function 'acp-send-request)
                (lambda (&rest args)
-                 (setq sent-request args))))
+                 (setq sent-request args)))
+              ((symbol-function 'agent-shell-viewport--buffer)
+               (lambda (&rest _args) nil)))
 
       ;; First, verify that build-content-blocks actually throws an error
       (should-error (agent-shell--build-content-blocks "Test prompt")
@@ -1149,25 +1159,30 @@ code block content
 
 (ert-deftest agent-shell--format-session-date-test ()
   "Test `agent-shell--format-session-date' humanizes timestamps."
-  ;; Today
-  (let* ((now (current-time))
-         (today-iso (format-time-string "%Y-%m-%dT10:30:00Z" now)))
-    (should (equal (agent-shell--format-session-date today-iso)
-                   "Today, 10:30")))
-  ;; Yesterday
-  (let* ((yesterday (time-subtract (current-time) (* 24 60 60)))
-         (yesterday-iso (format-time-string "%Y-%m-%dT15:45:00Z" yesterday)))
-    (should (equal (agent-shell--format-session-date yesterday-iso)
-                   "Yesterday, 15:45")))
-  ;; Same year, older
-  (should (string-match-p "^[A-Z][a-z]+ [0-9]+, [0-9]+:[0-9]+"
-                           (agent-shell--format-session-date "2026-01-05T09:00:00Z")))
-  ;; Different year
-  (should (string-match-p "^[A-Z][a-z]+ [0-9]+, [0-9]\\{4\\}"
-                           (agent-shell--format-session-date "2025-06-15T12:00:00Z")))
-  ;; Invalid input falls back gracefully
-  (should (equal (agent-shell--format-session-date "not-a-date")
-                 "not-a-date")))
+  (let ((fixed-now (date-to-time "2026-02-21T12:00:00Z")))
+    (cl-letf (((symbol-function 'current-time) (lambda () fixed-now)))
+      ;; Today
+      (let* ((today-time (date-to-time "2026-02-21T10:30:00Z"))
+             (today-expected (format-time-string "Today, %H:%M" today-time)))
+        (should (equal (agent-shell--format-session-date "2026-02-21T10:30:00Z")
+                       today-expected)))
+      ;; Yesterday
+      (let* ((yesterday-time (date-to-time "2026-02-20T15:45:00Z"))
+             (yesterday-expected (format-time-string "Yesterday, %H:%M" yesterday-time)))
+        (should (equal (agent-shell--format-session-date "2026-02-20T15:45:00Z")
+                       yesterday-expected)))
+      ;; Same year, older
+      (should (string-match-p "^[A-Z][a-z]+ [0-9]+, [0-9]+:[0-9]+"
+                              (agent-shell--format-session-date "2026-01-05T09:00:00Z")))
+      ;; Different year
+      (should (string-match-p "^[A-Z][a-z]+ [0-9]+, [0-9]\\{4\\}"
+                              (agent-shell--format-session-date "2025-06-15T12:00:00Z")))
+      ;; Invalid input falls back gracefully
+      (cl-letf (((symbol-function 'date-to-time)
+                 (lambda (_timestamp)
+                   (error "Unparseable timestamp"))))
+        (should (equal (agent-shell--format-session-date "not-a-date")
+                       "not-a-date"))))))
 
 (ert-deftest agent-shell--prompt-select-session-test ()
   "Test `agent-shell--prompt-select-session' choices."
@@ -1241,17 +1256,17 @@ code block content
   "Test that :outgoing-request-decorator from state reaches the ACP client."
   (with-temp-buffer
     (let* ((my-decorator (lambda (request) request))
-           (agent-shell--state (agent-shell--make-state
-                                :agent-config nil
-                                :buffer (current-buffer)
-                                :client-maker (lambda (_buffer)
-                                                (agent-shell--make-acp-client
-                                                 :command "cat"
-                                                 :context-buffer (current-buffer)))
-                                :outgoing-request-decorator my-decorator)))
+           (state (agent-shell--make-state
+                   :agent-config nil
+                   :buffer (current-buffer)
+                   :client-maker (lambda (_buffer)
+                                   (agent-shell--make-acp-client
+                                    :command "cat"
+                                    :context-buffer (current-buffer)))
+                   :outgoing-request-decorator my-decorator)))
       ;; setq-local needed for buffer-local-value in agent-shell--make-acp-client
-      (setq-local agent-shell--state agent-shell--state)
-      (let ((client (funcall (map-elt agent-shell--state :client-maker)
+      (setq-local agent-shell--state state)
+      (let ((client (funcall (map-elt state :client-maker)
                              (current-buffer))))
         (should (eq (map-elt client :outgoing-request-decorator) my-decorator))))))
 
@@ -1265,16 +1280,16 @@ code block content
                                     (cons '(_meta . ((systemPrompt . ((append . "extra instructions")))))
                                           (map-elt request :params))))
                         request))
-           (agent-shell--state (agent-shell--make-state
-                                :agent-config nil
-                                :buffer (current-buffer)
-                                :client-maker (lambda (_buffer)
-                                                (agent-shell--make-acp-client
-                                                 :command "cat"
-                                                 :context-buffer (current-buffer)))
-                                :outgoing-request-decorator decorator)))
-      (setq-local agent-shell--state agent-shell--state)
-      (let ((client (funcall (map-elt agent-shell--state :client-maker)
+           (state (agent-shell--make-state
+                   :agent-config nil
+                   :buffer (current-buffer)
+                   :client-maker (lambda (_buffer)
+                                   (agent-shell--make-acp-client
+                                    :command "cat"
+                                    :context-buffer (current-buffer)))
+                   :outgoing-request-decorator decorator)))
+      (setq-local agent-shell--state state)
+      (let ((client (funcall (map-elt state :client-maker)
                              (current-buffer))))
         ;; Give client a fake process so acp--request-sender proceeds
         (map-put! client :process (start-process "fake" nil "cat"))
@@ -1342,7 +1357,7 @@ code block content
   (should (null (agent-shell--extract-tool-parameters
                  '((plan . "Step 1: do something"))))))
 
-(ert-deftest agent-shell--make-transcript-tool-call-entry-test ()
+(ert-deftest agent-shell--make-transcript-tool-call-entry-parameters-test ()
   "Test `agent-shell--make-transcript-tool-call-entry' with parameters."
   ;; Test basic entry without parameters
   (let ((entry (agent-shell--make-transcript-tool-call-entry